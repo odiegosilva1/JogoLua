2d3d8edbad3e3b6bf6ddfde65541603ccc1defac
odiegosilva1 <odiegosilvadev@gmail.com>
2025-07-23 15:17:42 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,15 +2,16 @@
 _G.love = require("love")
 
  function love.load() -- Load the game
+      _G.number = 0
 
  end
 
   function love.update(dt) -- Update game - dt = Delta Time
-
+      number = number + 1
  end
 
   function love.draw()
-    love.graphics.print("Hello Mundo")
+    love.graphics.print(number)
 
  end
 
