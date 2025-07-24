ebaf1aa0420d7874e189c297ae69122612184b47
odiegosilva1 <odiegosilvadev@gmail.com>
2025-07-24 09:48:12 -0300

    update: main.lua
    add readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# JogoLua
- Jogo 2D feito usando LUA.
+# 📜 **Jogos em Lua com LÖVE (Love2D)**  
+
+Um repositório para projetos de jogos desenvolvidos em **Lua** usando o framework **LÖVE2D**.  
+Aqui você encontrará exemplos, tutoriais e projetos pessoais para aprender e compartilhar conhecimentos.  
+
+---
+
+  
+
+
--- main.lua
+++ main.lua
@@ -3,30 +3,33 @@ _G.love = require("love")
 
  function love.load() -- Load the game
      love.graphics.setBackgroundColor(1, 1, 1)
+
 -- Set player position
      _G.pacman = {}
      pacman.x = 200
      pacman.y = 250
-     pacman.eat = true
--- Food 
-     _G.food_X = 600
-     _G.food_eaten = false
 
+-- Food
+     _G.food = {
+          x = 600,
+          eaten = false
+     }
  end
 
   function love.update(dt)  -- Update game - dt = Delta Time
      pacman.x = pacman.x + 1
 
-     if pacman.x >= food_X + 20 then
-          _G.food_eaten = true
+     if pacman.x >= food.x + 20 then
+          food.eaten = true
+     
      end     
-       
  end
 
+
   function love.draw()
-     if not food_eaten then 
+     if not food.eaten then 
           love.graphics.setColor(0, 0, 0) 
-          love.graphics.rectangle("fill", food_X, 200, 70, 70)
+          love.graphics.rectangle("fill", food.x, 200, 70, 70)
      end
 
  
