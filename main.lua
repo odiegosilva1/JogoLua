f6fc48fc9fded538988eaf1c47b203514673feb3
odiegosilva1 <odiegosilvadev@gmail.com>
2025-07-24 10:53:08 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,6 +8,9 @@ _G.love = require("love")
      _G.pacman = {}
      pacman.x = 200
      pacman.y = 250
+     pacman.angle1 = 0.2
+     pacman.angle2 = 2 * math.pi -0.2
+     pacman.rotationSpeed = math.pi
 
 -- Food
      _G.food = {
@@ -17,6 +20,15 @@ _G.love = require("love")
  end
 
   function love.update(dt)  -- Update game - dt = Delta Time
+     -- Rotate player
+     if love.keyboard.isDown("down")  then 
+        pacman.angle1 = pacman.angle1 + pacman.rotationSpeed * dt
+        pacman.angle2 = pacman.angle2 + pacman.rotationSpeed * dt
+     elseif love.keyboard.isDown("up") then
+        pacman.angle1 = pacman.angle1 - pacman.rotationSpeed * dt
+        pacman.angle2 = pacman.angle2 - pacman.rotationSpeed * dt       
+     end
+
      -- Keyboard Input 
      if love.keyboard.isDown("a")  then 
           pacman.x = pacman.x - 1
@@ -49,7 +61,7 @@ _G.love = require("love")
  
 
     love.graphics.setColor(0, 0, 0)
-    love.graphics.arc("fill", pacman.x, pacman.y, 60, 1, 5)
+    love.graphics.arc("fill", pacman.x, pacman.y, 60, pacman.angle1, pacman.angle2)
 
  end
 
