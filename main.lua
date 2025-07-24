d31e649d965e11203edfe74b46df4653a2eebf6e
odiegosilva1 <odiegosilvadev@gmail.com>
2025-07-24 08:51:45 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,16 +2,36 @@
 _G.love = require("love")
 
  function love.load() -- Load the game
-      _G.number = 0
+     love.graphics.setBackgroundColor(1, 1, 1)
+-- Set player position
+     _G.pacman = {}
+     pacman.x = 200
+     pacman.y = 250
+     pacman.eat = false
+
+     _G.food_X = 600
 
  end
 
-  function love.update(dt) -- Update game - dt = Delta Time
-      number = number + 1
+  function love.update(dt)  -- Update game - dt = Delta Time
+     pacman.x = pacman.x + 1
+
+     if pacman.x >= food_X + 20 then
+          pacman.eat = true
+     end     
+       
  end
 
   function love.draw()
-    love.graphics.print(number)
+     if not pacman.eat then 
+          love.graphics.setColor(0, 0, 0) 
+          love.graphics.rectangle("fill", food_X, 200, 70, 70)
+     end
+
+ 
+
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.arc("fill", pacman.x, pacman.y, 60, 1, 5)
 
  end
 
