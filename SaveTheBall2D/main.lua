c4f84618b43484dfa9a8edbb005e216d2b870be3
odiegosilva1 <odiegosilvadev@gmail.com>
2025-07-26 22:13:23 -0300

    update:main.lua

--- SaveTheBall2D/main.lua
+++ SaveTheBall2D/main.lua
@@ -1,12 +1,35 @@
 local love = require "love"
 
+local game = {
+  state = {
+     menu = true,
+     paused = false,
+     running = false,
+     ended = false,
+
+  }
+
+}
+
+
+local player = {
+    radius = 20,
+    x = 30,
+    y = 30
+
+}
+
 function love.load()
+  love.window.setTitle("Salve a Bola")
+
 end
 
 
 function love.update()
+    player.X, player.y = love.mouse.getPosition()
 end
 
 
 function love.draw()
+  love.graphics.circle("fill", player.x, player.y, player.radius)
 end
